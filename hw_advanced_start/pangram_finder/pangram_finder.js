0c611355fc15906bddc565abdb28f051bb0ecf64
crispymccoy145 <samspam19941@gmail.com>
2021-05-05 17:00:30 +0100

    completed first panagram extension

--- hw_advanced_start/pangram_finder/pangram_finder.js
+++ hw_advanced_start/pangram_finder/pangram_finder.js
@@ -1,9 +1,17 @@
 const PangramFinder = function (phrase) {
   this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
+  this.phrase = phrase;
 }
 
 PangramFinder.prototype.isPangram = function () {
-
-}
+  let splitPhrase = this.phrase.split('');
+  splitPhrase = splitPhrase.filter((item) => {
+    return item != ' ';
+  });
+  const answer = splitPhrase.every((item) => {
+    return this.alphabet.includes(item);
+  });
+  return answer;
+};
 
 module.exports = PangramFinder;
